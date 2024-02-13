64c79c0fcdaaf28729c699be54128809c519e435
jww2145 <jww2145@columbia.edu>
2024-02-12 21:35:08 -0500

    Added header

---
+++ src/components/Header.svelte
@@ -0,0 +1,35 @@
+<div class = 'header'></div>
+<p class = 'intro'>Created for DevFest 2024</p>
+<h1>Environmental Politics by the Numbers</h1>
+<p class = 'intro'>By <b>Joshua Wu</b> and <b>Brian Echavarria</b></p>
+<div class = 'header'></div>
+
+
+<style>
+    h1{
+        color: black;
+        font-family: Georgia;
+        text-align: center;
+    }
+
+    .header{
+        margin: auto;
+        padding-bottom: 3%;
+    }
+
+    .intro{
+        text-align: center;
+        font-family: 'Courier New', Courier, monospace;
+        color: black;
+        font-size: smaller;
+    }
+
+    b{
+        font-weight: 1500;
+        color: #005682;
+    }
+
+    ::selection {
+        background: #D8F1FF;
+    }
+</style>
